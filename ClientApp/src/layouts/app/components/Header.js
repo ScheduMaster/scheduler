0c14b658d926f15e30afcdf8c7492a47c2c4ad2b
Võ Quý Long <long@idctechno.com>
2023-04-26 22:51:26 +0700

    Implement header of app layout

--- ClientApp/src/layouts/app/components/Header.js
+++ ClientApp/src/layouts/app/components/Header.js
@@ -1,1 +1,33 @@
-F
+import React, { Component } from "react";
+import { Logo } from "./Header/Logo";
+import { Notification } from "./Header/Notification";
+import { ShortCutList } from "./Header/ShortCutList";
+import { Avatar } from "./Header/Avatar";
+
+export class Header extends Component {
+    render () {
+
+        return (
+            <header className="navbar navbar-expand-md navbar-light d-print-none">
+                <div className="container-xl">
+                    {/* Logo component */}
+                    <Logo src={this.props.logo.src} name={this.props.logo.name}/>
+                    <div className="navbar-nav flex-row order-md-last">
+                        <div className="d-none d-md-flex">
+                            {/* ShortcutList compoent */}
+                            <ShortCutList shortcutsData={this.props.shortcutsData}/>
+                            {/* Notification component */}
+                            <Notification notifications={this.props.notificationsData}/>
+                        </div>
+                        {/* Avatar component */}
+                        <Avatar 
+                            name={this.props.avatar.name}
+                            role={this.props.avatar.role}
+                            dropdowns={this.props.avatar.dropdowns}
+                        />
+                    </div>
+                </div>
+            </header>
+        )
+    }
+}
