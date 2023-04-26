cd5bcd7d8b7f19046ca905b30dee62c62a0e2be9
Võ Quý Long <long@idctechno.com>
2023-04-26 22:50:36 +0700

    Implement app layout

---
+++ ClientApp/src/layouts/app/Application.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+
+// Components
+import { Header } from "./components/Header";
+import { Footer } from "./components/Footer";
+import { Navbar } from "./components/Narbar";
+
+// Styles
+import '@tabler/core/dist/css/tabler.min.css';
+
+// Data Header
+import { shortcutsData } from "./data/shortcut";
+import { notificationsData } from "./data/notification";
+import { logo } from "./data/logo";
+import { avatar } from "./data/avatar";
+
+// Data Navbar
+import { navbarsData } from "./data/navbar";
+
+// Data footer
+import { footersData } from "./data/footer";
+
+export class Application extends Component {
+    render () {
+        const { children } = this.props;
+        
+        return (
+            <div className="page">
+                <Header 
+                    shortcutsData={shortcutsData} 
+                    notificationsData={notificationsData}
+                    logo={logo}
+                    avatar={avatar}
+                />
+                <Navbar navbarsData={navbarsData}/>
+                <div className="page-wrapper">
+                    {children}
+                    <Footer footersData={footersData}/>
+                </div>
+            </div>
+        ) 
+    }
+}
