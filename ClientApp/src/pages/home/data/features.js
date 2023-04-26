4f978ab58d7ad0613bc9528b30b95fc9ab14cf50
Võ Quý Long <long@idctechno.com>
2023-04-26 22:54:11 +0700

    Update new path of data folder

---
+++ ClientApp/src/pages/home/data/features.js
@@ -0,0 +1,22 @@
+export const featuresData = [
+    {
+        title: "Free and Open-Source",
+        desc: "Lorem Ipsum is simply dummy text of the printing and industry.",
+        link: "#",
+    },
+    {
+        title: "Customizable Design",
+        desc: "Lorem Ipsum is simply dummy text of the printing and industry.",
+        link: "#",
+    },
+    {
+        title: "Responsive Layout",
+        desc: "Lorem Ipsum is simply dummy text of the printing and industry.",
+        link: "#",
+    },
+    {
+        title: "Responsive Layout",
+        desc: "Lorem Ipsum is simply dummy text of the printing and industry.",
+        link: "#",
+    }
+];
---
+++ ClientApp/src/pages/home/data/questions.js
@@ -0,0 +1,30 @@
+export const questionsData = [
+    [
+        {
+            question: "How to use UIdeck?",
+            answer: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+        },
+        {
+            question: "How to download icons from Lineicons?",
+            answer: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+        },
+        {
+            question: "Is GrayGrids part of UIdeck?",
+            answer: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+        },
+    ],
+    [
+        {
+            question: "Can I use this template for commercial project?",
+            answer: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+        },
+        {
+            question: "Can I use this template for commercial project?",
+            answer: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+        },
+        {
+            question: "Can I use this template for commercial project?",
+            answer: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+        }
+    ]
+];
