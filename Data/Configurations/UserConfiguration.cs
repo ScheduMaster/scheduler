17fb88f974e126778390a9c4c7054fcf434e3ecc
Võ Quý Long <long@idctechno.com>
2023-04-30 19:23:30 +0700

    User configuration

---
+++ Data/Configurations/UserConfiguration.cs
@@ -0,0 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Application.Data.Entities;
+
+namespace Application.Data
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.ToTable("USER");
+            builder.HasKey(user => user.Id);
+            builder.Property(user => user.FirstName);
+            builder.Property(user => user.LastName);
+            builder.Property(user => user.Address);
+            builder.Property(user => user.Email);
+            builder.Property(user => user.Role);
+            builder.Property(user => user.DateOfBirth);
+            builder.Property(user => user.IsActive);
+        }
+    }
+}
