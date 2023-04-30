b7d3de0938a25592cd26bc423a512e45128899c1
Võ Quý Long <long@idctechno.com>
2023-04-30 19:24:07 +0700

    User model

---
+++ Models/User.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Application.Data.Entities 
+{
+    public class User : IdentityUser<Guid>
+
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+        public string Address { get; set; }
+        public string Role { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public bool IsActive { get; set; }
+    }
+
+}
