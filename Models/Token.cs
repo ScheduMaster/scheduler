c709c035d983bc38066af1bc59cb978293204f9f
Võ Quý Long <long@idctechno.com>
2023-05-02 00:04:57 +0700

    Token model

---
+++ Models/Token.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace Application.Models
+{
+    public enum TokenType
+    {
+        ACCESS,
+        REFRESH
+    } 
+    public class AuthToken
+    {
+        public int Id { get; set; }
+        public string Token { get; set; }
+        public TokenType Type { get; set; }
+        public DateTime ExpiresAt { get; set; }
+        public Guid UserId { get; set; }
+        public bool Blacklisted { get; set; }
+    }
+}
