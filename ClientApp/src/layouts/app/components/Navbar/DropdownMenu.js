ec5bbb6ed4514bba73fd6e5528eac12fec328f64
Võ Quý Long <vqlong1604@gmail.com>
2023-05-09 04:44:36 +0900

    Remove unuse code

--- ClientApp/src/layouts/app/components/Navbar/DropdownMenu.js
+++ ClientApp/src/layouts/app/components/Navbar/DropdownMenu.js
@@ -2,18 +2,6 @@ import React, { Component } from "react";
 import { DropdownItem } from "./DropdownItem";
 
 export class DropdownMenu extends Component {
-    // constructor(props) {
-    //     super(props);
-    //     const { dropdownsData } = props;
-    
-    //     const dropdownsMenu = [];
-    //     for (let i = 0; i < dropdownsData.length; i += 2) {
-    //       const chunk = dropdownsData.slice(i, i + 2);
-    //       dropdownsMenu.push(chunk);
-    //     }
-    
-    //     this.state = { dropdownsMenu };
-    // }
 
     render () {
         const { dropdownsData } = this.props;
