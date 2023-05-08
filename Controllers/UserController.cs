8577488949ad580fdff5210de868b306d86af8ca
Võ Quý Long <vqlong1604@gmail.com>
2023-05-09 04:49:05 +0900

    Update controller with userService

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -4,12 +4,12 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
-using Application.Data;
 using Application.Data.Entities;
 using Application.Services;
-
+using Microsoft.AspNetCore.Http;
 namespace Application.Controllers
 {
+    [Authorize]
     [ApiController]
     [Route("api/[controller]")]
     public class UserController : ControllerBase
@@ -17,12 +17,14 @@ namespace Application.Controllers
         private readonly IConfiguration _config;
         private readonly ITokenService _tokenService;
         private readonly IAuthService _authService;
+        private readonly IUserService _userService;
 
-        public UserController(IConfiguration config, ITokenService tokenService, IAuthService authService)
+        public UserController(IConfiguration config, ITokenService tokenService, IAuthService authService, IUserService userService)
         {
             _config = config;
             _tokenService = tokenService;
             _authService = authService;
+            _userService = userService;
         }
 
         [HttpGet("info")]
@@ -30,23 +32,24 @@ namespace Application.Controllers
         {
             try
             {
-                string token = HttpContext.Request.Headers["Authorization"].ToString().Substring("Bearer ".Length).Trim();
-                User user = _tokenService.GetUserInfo(token);
+                // string token = HttpContext.Request.Headers["Authorization"].ToString().Substring("Bearer ".Length).Trim();
+                string UserId = (string)HttpContext.Items["UserId"];
+                User user = _userService.GetUserInfo(Guid.Parse(UserId));
 
                 if (user == null)
                 {
-                    return NotFound("User not found");
+                    return NotFound(new { message = "User not found" });
                 }
 
                 return Ok(new 
                     {
-                        name = user.FirstName,
-                        role = "Administrator"
+                        name = user.GetUsername(),
+                        role = user.Role
                     });
             }
             catch (Exception ex)
             {
-                return BadRequest(ex.Message);
+                return BadRequest(new { message = ex.Message });
             }
         }
     }   
