8bcd7939840b5908afe5f5e3e96878f57e2e5cc2
Võ Quý Long <vqlong1604@gmail.com>
2023-05-16 10:08:45 +0900

    Data table component

--- ClientApp/src/pages/app/components/DataTable.js
+++ ClientApp/src/pages/app/components/DataTable.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import { Table, Pagination, Dropdown } from 'react-bootstrap';
+import { ActionDropdown } from "./ActionDropdown";
 import '../static/css/data-table.css';
+import { actions } from '../data/calendar';
 
 export class DataTable extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      entriesPerPage: 10,
+      entriesPerPage: 5,
       activePage: 1
     };
 
@@ -37,63 +39,72 @@ export class DataTable extends Component {
     const endIndex = startIndex + entriesPerPage;
     const currentEntries = entries.slice(startIndex, endIndex);
     
-      return (
-        <>
-          <div className="table-responsive">
-            <Table bordered hover >
-              <thead>
-                <tr>
-                  <th>NO.</th>
-                  <th>Title</th>
-                  <th>Client</th>
-                  <th>Location</th>
-                  <th>Status</th>
-                  <th />
+    return (
+      <>
+        <div className="table-responsive">
+          <Table bordered hover >
+            <thead>
+              <tr>
+                <th>NO.</th>
+                <th>Title</th>
+                <th>Client</th>
+                <th>Location</th>
+                <th>Start</th>
+                <th>End</th>
+                <th>Status</th>
+                <th />
+              </tr>
+            </thead>
+            <tbody>
+              {currentEntries.map(({ id, title, client, location, start, end, status }) => (
+                <tr key={id}>
+                  <td>{id}</td>
+                  <td>{title}</td>
+                  <td>{client}</td>
+                  <td>{location}</td>
+                  <td>{start.toLocaleString()}</td>
+                  <td>{end.toLocaleString()}</td>
+                  <td>{status}</td>
+                  <td className="text-end">
+                    <ActionDropdown actions={actions}/>
+                  </td>
                 </tr>
-              </thead>
-              <tbody>
-                {currentEntries.map(({ id, name, email }) => (
-                  <tr key={id}>
-                    <td>{id}</td>
-                    <td>{name}</td>
-                    <td>{email}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </Table>
-          </div>
-
-          <div className="d-flex justify-content-between align-items-center">
-            <Dropdown>
-              <Dropdown.Toggle variant="light" id="dropdown-entries">
-                Entries per page: {entriesPerPage}
-              </Dropdown.Toggle>
-              <Dropdown.Menu>
-                {[10, 25, 50].map((entries) => (
-                  <Dropdown.Item
-                    key={entries}
-                    active={entries === entriesPerPage}
-                    onClick={() => this.handleSelectEntries(entries)}
-                  >
-                    {entries}
-                  </Dropdown.Item>
-                ))}
-              </Dropdown.Menu>
-            </Dropdown>
-
-            <Pagination>
-              {[...Array(pageCount)].map((_, index) => (
-                <Pagination.Item
-                  key={index}
-                  active={index + 1 === activePage}
-                  onClick={() => this.handleSelectPage(index + 1)}
-                >
-                  {index + 1}
-                </Pagination.Item>
               ))}
-            </Pagination>
-          </div>
-        </>
-      );
-    }
+            </tbody>
+          </Table>
+        </div>
+
+        <div className="d-flex justify-content-between align-items-center">
+          <Dropdown>
+            <Dropdown.Toggle variant="light" id="dropdown-entries">
+              Entries per page: {entriesPerPage}
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+              {[10, 25, 50].map((entries) => (
+                <Dropdown.Item
+                  key={entries}
+                  active={entries === entriesPerPage}
+                  onClick={() => this.handleSelectEntries(entries)}
+                >
+                  {entries}
+                </Dropdown.Item>
+              ))}
+            </Dropdown.Menu>
+          </Dropdown>
+
+          <Pagination>
+            {[...Array(pageCount)].map((_, index) => (
+              <Pagination.Item
+                key={index}
+                active={index + 1 === activePage}
+                onClick={() => this.handleSelectPage(index + 1)}
+              >
+                {index + 1}
+              </Pagination.Item>
+            ))}
+          </Pagination>
+        </div>
+      </>
+    );
   }
+}
