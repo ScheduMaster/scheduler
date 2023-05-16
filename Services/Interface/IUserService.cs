c12db0a1204d29b17af236981493caa3ef88ccb3
Võ Quý Long <vqlong1604@gmail.com>
2023-05-16 10:14:20 +0900

    Update user service interface

--- Services/Interface/IUserService.cs
+++ Services/Interface/IUserService.cs
@@ -1,5 +1,6 @@
 using Application.Data.Entities;
 using System.Threading.Tasks;
+using Application.Models.Requests;
 using System;
 
 namespace Application.Services
@@ -8,6 +9,10 @@ namespace Application.Services
     {
         // Login a user and return an access token
         User GetUserInfo(Guid UserId);
+        Task<User> CreateUserAsync(CreateUserModel CreateUserModel);
+        Task<User> UpdatePasswordAsync(User user, string NewPassword);
+        Task<User> UpdateUserAsync(User user, UpdateUserModel model);
+        Task<User> UpdateUserAsync(User user, UpdateProfileModel model);
+        Task<bool> DeleteUserAsync(User user);
     }
-
 }
