e2f5ce1551ebc0735ce2448c4fbcf1049a0706b5
Võ Quý Long <vqlong1604@gmail.com>
2023-05-16 10:03:12 +0900

    Update path of components

---
+++ ClientApp/src/components/AppRoute.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import { Route, Redirect } from "react-router-dom";
+import AuthContext from './AuthContext';
+
+export class AppRoute extends Component {
+  static contextType = AuthContext;
+  
+  render() {
+    const { accessToken, refreshToken } = this.context;
+    const { component: Component, layout: Layout, path: Path, ...rest } = this.props;
+    const { type } = this.props;
+
+    if (type === 'authenticated') {
+      if (!accessToken && !refreshToken) {
+        return <Redirect to="/auth/login" />;
+      }
+      if (accessToken || refreshToken) {
+        return <Route {...rest} render={props => (
+          <Layout>
+            <Component {...props} />
+          </Layout>
+        )} />;
+      }
+    } else if (type === "non-authenticated") {
+        return <Route {...rest} render={props => (
+          <Layout>
+            <Component {...props} />
+          </Layout>
+        )} />;
+    }
+  }
+}
