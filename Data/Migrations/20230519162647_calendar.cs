961975b05cbdf97e2ab60c3725456c69df50d45d
Võ Quý Long <long@idctechno.com>
2023-05-20 08:22:01 +0700

    Update calendar table

---
+++ Data/Migrations/20230519162647_calendar.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace scheduler.Data.Migrations
+{
+    public partial class calendar : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "calendars",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    user_id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    background_color = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    border_color = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    drag_background_color = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_calendars", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_calendar_user",
+                        column: x => x.user_id,
+                        principalTable: "USER",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_calendars_user_id",
+                table: "calendars",
+                column: "user_id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "calendars");
+        }
+    }
+}
