dfb98de462d0f19f64d6b06bc3bbb1b073cc96b8
Võ Quý Long <long@idctechno.com>
2023-05-21 15:17:52 +0700

    Update work provider virtual attribute

--- Data/Entities/WorkProvider.cs
+++ Data/Entities/WorkProvider.cs
@@ -1,14 +1,14 @@
 using System;
-using Application.Data.Entities;
+using System.Collections.Generic;
 
 namespace Application.Data.Entities
 {
     public class WorkProvider
     {
         public Guid UserId { get; set; }
-        public int WorkId { get; set; }
+        public int AppointmentId { get; set; }
 
-        public User User { get; set; }
-        public Work Work { get; set; }
+        public virtual User User { get; set; }
+        public virtual Appointment Appointment { get; set; }
     }
 }
