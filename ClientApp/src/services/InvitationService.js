7c7abca6666a92e672613964227d5b18536ebc59
Võ Quý Long <long@idctechno.com>
2023-05-27 23:39:50 +0700

    Update folder form 'Services' to 'services'

---
+++ ClientApp/src/services/InvitationService.js
@@ -0,0 +1,57 @@
+import { JwtInterceptor } from './JwtInterceptor';
+
+export class InvitationService {
+  constructor() {
+    this.interceptor = new JwtInterceptor();
+  }
+
+  generateInvitation = async (appointmentId) => {
+    const data = await this.interceptor.post(`/api/invitation/generate`, 
+    {
+      appointmentId: appointmentId
+    },
+    {
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    });
+
+    return data;
+  }
+
+  getInvitation = async (appointmentId) => {
+    const data = await this.interceptor.get(`/api/invitation/fetch/${appointmentId}`, {},
+    {
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    });
+
+    return data;
+  }
+
+  checkInvitation = async (invitationId) => {
+    const data = await this.interceptor.post(`/api/invitation/check`, 
+    {
+        invitationId: invitationId
+    },
+    {
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    });
+
+    return data;
+  }
+
+  acceptInvitation = async (invitationId) => {
+    const data = await this.interceptor.get(`/api/invitation/accept/${invitationId}`, {},
+    {
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    });
+
+    return data;
+  }
+}
