295bf14f34358dc64fc1725f19c36eba5a701bc1
Võ Quý Long <long@idctechno.com>
2023-05-27 21:14:17 +0700

    Update commit and hash

--- send-telegram-notification.js
+++ send-telegram-notification.js
@@ -3,14 +3,16 @@ const fs = require('fs');
 const { promisify } = require('util');
 const readFileAsync = promisify(fs.readFile);
 
-async function sendTelegramMessage(repository, actor, eventName) {
+async function sendTelegramMessage(repository, actor, eventName, commitMessage, commitHash) {
   const markdownTemplate = await readFileAsync('message-template.md', 'utf8');
 
   const message = markdownTemplate
     .replace('{{repository}}', repository)
     .replace('{{actor}}', actor)
     .replace('{{eventName}}', eventName)
-    .replace('{{time}}', new Date().toISOString());
+    .replace('{{time}}', new Date().toISOString())
+    .replace('{{commitMessage}}', commitMessage)
+    .replace('{{commitHash}}', commitHash);
 
   const requestBody = {
     chat_id: process.env.TELEGRAM_TO,
@@ -28,4 +30,4 @@ async function sendTelegramMessage(repository, actor, eventName) {
   }
 }
 
-sendTelegramMessage(process.argv[2], process.argv[3], process.argv[4]);
+sendTelegramMessage(process.argv[2], process.argv[3], process.argv[4], process.argv[5], process.argv[6]);
