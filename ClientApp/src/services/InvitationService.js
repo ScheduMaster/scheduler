d69a35d07e0006418bd70c6f820efe0daf19afe5
Võ Quý Long <long@idctechno.com>
2023-06-04 13:59:11 +0700

    Handle call api join invitation

--- ClientApp/src/services/InvitationService.js
+++ ClientApp/src/services/InvitationService.js
@@ -29,4 +29,9 @@ export class InvitationService {
     const data = await this.interceptor.post(`/api/invitation/send`, { userId, appointmentId }, {});
     return data;
   }
+
+  joinInvitation = async (invitationId) => {
+    const data = await this.interceptor.get(`/api/invitation/join/${invitationId}`, {}, {});
+    return data;
+  }
 }
