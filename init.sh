58519e6e27fb9aa9aa4405fdf96cf9fbaae5f20c
Võ Quý Long <long@idctechno.com>
2023-06-28 01:12:05 +0700

    update

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
-docker exec -it scheduler-database-1 /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Thisisasecret6969
+docker exec -it scheduler_database_1 /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Thisisasecret6969
 CREATE DATABASE scheduler;
 Go
 QUIT
